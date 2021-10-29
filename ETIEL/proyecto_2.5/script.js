b9baa9993653fe08888df08535202f5c9ec4e920
krlocho <thekrl88@gmail.com>
2021-10-29 09:13:36 +0100

    The script almost works perfectly

--- ETIEL/proyecto_2.5/script.js
+++ ETIEL/proyecto_2.5/script.js
@@ -26,36 +26,34 @@ this.buy = function(seatNumber){
     var row = Math.floor((seatNumber) / this.colum);
     var col = seatNumber % this.colum;
     if (this.seats[row][col] == 1){
-        let index = mySeats.indexOf(seatNumber)
+        let index = seatsToBuy.indexOf(seatNumber) //this gives youu the index of the seat
         if (index != -1) {
-            allRows[seatNumber].innerHTML = "<img src='../img/seat-green.png' alt='Green available seat' onclick='myFilm.buy(" + seatNumber + ")'><p>" + seatNumber + "</p>" //The seat returns to available state
-            this.seats[row][col] = 0;   //Now the seat is free
-            myPrice = this.price; //Price updated
-            mySeats.splice(index, 1);   //If the seat number is taken by the same user, that seat now is removed from the array of selected seats
-            document.getElementById("result1").innerHTML = myPrice + "€ ";  //Price to the user updated
-            tempCad = "";
-            for (let i = 0; i < mySeats.length; i++) {
-                tempCad += mySeats[i] + ", ";
+            allRows[seatNumber].innerHTML = "<img src='../img/seat-green.png' alt='available seat' onclick='exampleFilm.buy(" + seatNumber + ")'><p>" + seatNumber + "</p>" //The seat returns to available state
+            this.seats[row][col] = 0;   //empty the seat
+            totalPrice -= this.price; 
+            seatsToBuy.splice(index, 1);   //If the seat number is taken by the same user, that seat now is removed from the array of selected seats
+            document.getElementById("totalprice").innerHTML = totalPrice + "€ ";  //total price
+            tempfilmSelected = "";
+            for (let i = 0; i < seatsToBuy.length; i++) {
+                tempfilmSelected += seatsToBuy[i] + ", ";
             }
-            document.getElementById("result2").innerHTML = tempCad;
+            document.getElementById("seatsBought").innerHTML = tempfilmSelected;
         }
-        else {
-            alert("The seat " + seatNumber + " is taken!!\nPick another one!");
-        }
+       
 
     } 
     
     else if (this.seats[row][col] == 0){
         this.seats[row][col] = 1;
-        allRows[seatNumber].innerHTML = "<img src='../img/seat-red.png' alt='Red not available seat' onclick='myFilm.buy(" + seatNumber + ")'><p>" + seatNumber + "</p>";
-        myPrice += this.price;
-        document.getElementById("result1").innerHTML = myPrice + "€ ";
-        mySeats.push(seatNumber);
-        if (mySeats.length != 1) {  //If only for formatting purposes
-            document.getElementById("result2").innerHTML += ", " + mySeats[mySeats.length - 1];
+        allRows[seatNumber].innerHTML = "<img src='../img/seat-red.png' alt='not available seat' onclick='exampleFilm.buy(" + seatNumber + ")'><p>" + seatNumber + "</p>";
+        totalPrice += this.price;
+        document.getElementById("totalprice").innerHTML = totalPrice + "€ ";
+        seatsToBuy.push(seatNumber);
+        if (seatsToBuy.length != 1) {  //If only for formatting purposes
+            document.getElementById("seatsBought").innerHTML += ", " + seatsToBuy[seatsToBuy.length - 1];
         }
         else {
-            document.getElementById("result2").innerHTML += mySeats[mySeats.length - 1];                
+            document.getElementById("seatsBought").innerHTML += seatsToBuy[seatsToBuy.length - 1];                
         }
     }
 }
@@ -75,60 +73,57 @@ shangChiFilm = new film("Shang Chi ", 7, 4, 11)
 spaceJamFilm = new film("Space Jam  ", 12, 3, 9)
  
 
-myFilm = new film('null', 'null', 0, 0, 0); 
-
-var myPrice = 0;    // Total price of the tickets
-var mySeats = [];   // Array of the client is buying
+exampleFilm = new film('null', 'null', 0, 0, 0) //example film that is going to be replaced by the new one
 
 
-function aMovie(cad){
-    let cad1 = cad;
-    switch(cad) {
+//this is gonna be updated
+var totalPrice = 0;    
+var seatsToBuy = [];  
+
+
+function printMovie(filmSelected){
+    let filmSelected1 = filmSelected;
+    switch(filmSelected) {
         case "blackwidow":
-            if (sessionStorage.blackwidowSeatArray != undefined) {
-                blackWidowFilm.seats = JSON.parse(sessionStorage.blackwidowSeatArray);
-            }
-            else if (blackWidowFilm.seats.length == 0){
-                blackWidowFilm.createSeats();
-            }
-            myFilm = blackWidowFilm;
+     
+            blackWidowFilm.createSeats();
+            
+            exampleFilm = blackWidowFilm;
             break;
 
             case "shangchi":
-            if (sessionStorage.shangchiSeatArray != undefined) {
-                shangChiFilm.seats = JSON.parse(sessionStorage.shanChiSeatArray);
-            }
-            else if (shangChiFilm.seats.length == 0){
-                shangChiFilm.createSeats();
-            }
-            myFilm = shangChiFilm;
+          
+            shangChiFilm.createSeats()
+            exampleFilm = shangChiFilm;
             break;
-        
-            default:
-                alert('The movie name is invalid!!');
-        }
-document.write("<div id='price'>Total cost: </div>");
-document.write("<div id='result1'>0€</div>");
-document.write("<div id='yourSeats'>Your seats are: </div>");
-document.write("<div id='result2'></div>");
-document.write("<button id='buy' onclick='saveValues(" + JSON.stringify(cad) + ")'>Buy</button>");
-document.write("<button id='reset' onclick='resetValues()'>Reset</button>")
 
-document.write( "<div class='cont'>");
+            case "spacejam":
+         
+            spaceJamFilm.createSeats();
+            exampleFilm = spaceJamFilm;
+            break;
+    
+        }
+document.write("<div id='price'>Price of the seats: </div>");
+document.write("<div id='totalprice'>0€</div>");
+document.write("<div id='yourSeats'>Your selected the seats: </div>");
+document.write("<div id='seatsBought'></div>");
+document.write("<input type='button' onclick='location.reload();' value='Reset' />")
+document.write( "<div >");
 
 document.write("<table>");
 let seatsOfTheTheatre = 0;
-for(let i = 0; i < myFilm.row; i++) {
+for(let i = 0; i < exampleFilm.row; i++) {
     document.write("<tr>");
-    for(let j = 0; j < myFilm.colum; j++) {
-        if (myFilm.seats[i][j] == 0) {
-                document.write("<td class='spacer'><img src='../img/seat-green.PNG' alt='Green available seat' onclick='myFilm.buy(" + seatsOfTheTheatre + ")'><p>" + seatsOfTheTheatre + "</p></td>");
+    for(let j = 0; j < exampleFilm.colum; j++) {
+        if (exampleFilm.seats[i][j] == 0) {
+                document.write("<td ><img src='../img/seat-green.png' alt='available seat' onclick='exampleFilm.buy(" + seatsOfTheTheatre + ")'><p>" + seatsOfTheTheatre + "</p></td>");
                 seatsOfTheTheatre++;
             }
         
-        else if (myFilm.seats[i][j] == 1) {
+        else if (exampleFilm.seats[i][j] == 1) {
           
-                document.write("<td><img src='../img/seat-red.PNG' alt='Red not available seat' onclick='myFilm.buy(" + seatsOfTheTheatre + ")'><p>" + seatsOfTheTheatre + "</p></td>");
+                document.write("<td><img src='../img/seat-red.png' alt='not available seat' onclick='exampleFilm.buy(" + seatsOfTheTheatre + ")'><p>" + seatsOfTheTheatre + "</p></td>");
                 seatsOfTheTheatre++;
             }
         }
