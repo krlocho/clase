3a0aa9ad0f1f326107aa85e3046e9ff26a17788b
krlocho <thekrl88@gmail.com>
2021-11-08 13:07:57 +0000

    The timer and the state checker works

--- ETIEL/proyecto_4/script.js
+++ ETIEL/proyecto_4/script.js
@@ -1,81 +1,102 @@
+
 var security = 0
 var counter = 0
-var timerTime = new Date(); //Date object
+var timerTime = new Date();
 console.log(timerTime);
 
-var ms = 0; // milliseconds
-var s = 0;  // seconds
-var m = 0;  // minutes
+var miliSeconds = 0;
+var seconds = 0;
+var minutes = 0;
+
 
 var myTime = "00:00:00"
 
 
-var top5Records = []; // -> [username] || Used to store best 5 times
-//                          [minutes]
-//                          [seconds]
-//                          [miliseconds]
-//                          [moves]
-localStorage.records; //Stores best 5 times in local memory
-
-if (localStorage.records != undefined) {
-  top5Records = JSON.parse(localStorage.records);
-}
-
 function timer() {   //This is the timer
   var msAux, sAux, mAux;
-  ms += 2;
-  if (ms >= 100) {
-    s++;
-    ms = 0;
+  miliSeconds += 2;
+
+  if (miliSeconds >= 100) {
+    seconds++;
+    miliSeconds = 0;
   }
-  if (s > 59) {
-    m++;
-    s = 0;
+  
+  if (seconds > 59) {
+    minutes++;
+    seconds = 0;
   }
 
-  if (s < 10) { sAux = "0" + s; } else { sAux = s; }
-  if (m < 10) { mAux = "0" + m; } else { mAux = m; }
-  if (ms < 10) { msAux = "0" + ms; } else { msAux = ms; }
+
+  if (seconds < 10) { sAux = "0" + seconds; } else { sAux = seconds; }
+  if (minutes < 10) { mAux = "0" + minutes; } else { mAux = minutes; }
+  if (miliSeconds < 10) { msAux = "0" + miliSeconds; } else { msAux = miliSeconds; }
 
   document.getElementById("timer").innerHTML = mAux + ":" + sAux + ":" + msAux;
 }
 
-function swapTiles(cell1, cell2) {
+
+
+
+function movePics(cell1, cell2) {
   var temp = document.getElementById(cell1).className;
   document.getElementById(cell1).className = document.getElementById(cell2).className;
   document.getElementById(cell2).className = temp;
 }
+
+
+
 function resetValues() {
   counter = 0
+  miliSeconds = 0;
+  seconds = 0;
+  minutes = 0;
   
+
+  document.getElementById("movements").innerHTML = "Moves: " + counter;
+
+  document.getElementById("extra").innerHTML = " ";
+
+  document.getElementById("button").innerHTML = '<button onClick="shuffle();">Start</button> ';
+
+
+
 }
+
 function shuffle() {
-  security = 1
+  security++
   resetValues()
+  timer();
+  myTime = setInterval(timer, 20)
 
-  //Use nested loops to access each cell of the 3x3 grid
-  for (var row = 1; row <= 3; row++) { //For each row of the 3x3 grid
-    for (var column = 1; column <= 3; column++) { //For each column in this row
+  //coment this to don move the tiles randomly 
 
-      var row2 = Math.floor(Math.random() * 3 + 1); //Pick a random row from 1 to 3
-      var column2 = Math.floor(Math.random() * 3 + 1); //Pick a random column from 1 to 3
 
-      swapTiles("cell" + row + column, "cell" + row2 + column2); //Swap the look & feel of both cells
-    }
-  }
+    //access to each cell of the 3x3 grid
+    for (var row = 1; row <= 3; row++) { //For each row of the 3x3 grid
+      for (var column = 1; column <= 3; column++) { //For each column in this row
+  
+        var row2 = Math.floor(Math.random() * 3 + 1); //Pick a random row from 1 to 3
+        var column2 = Math.floor(Math.random() * 3 + 1); //Pick a random column from 1 to 3
+  
+        movePics("cell" + row + column, "cell" + row2 + column2); //Swap the look & feel of both cells
+      }
+    } 
 }
+
+
 
 function clickTile(row, column) {
 
   var cell = document.getElementById("cell" + row + column);
   var tile = cell.className;
   var moves = document.getElementById("movements").innerHTML = "Moves: " + counter;
- // if (security == 1) {
+
+  if (security >= 1) {
     if (tile != "tile9") {
       //Checking if white tile on the right
       if (column < 3) {
         if (document.getElementById("cell" + row + (column + 1)).className == "tile9") {
-          swapTiles("cell" + row + column, "cell" + row + (column + 1));
+          movePics("cell" + row + column, "cell" + row + (column + 1));
           counter++;
           solved();
           return;
@@ -84,7 +105,7 @@ function clickTile(row, column) {
       //Checking if white tile on the left
       if (column > 1) {
         if (document.getElementById("cell" + row + (column - 1)).className == "tile9") {
-          swapTiles("cell" + row + column, "cell" + row + (column - 1));
+          movePics("cell" + row + column, "cell" + row + (column - 1));
           counter++;
           console.log(counter); solved();
 
@@ -94,7 +115,7 @@ function clickTile(row, column) {
       //Checking if white tile is above
       if (row > 1) {
         if (document.getElementById("cell" + (row - 1) + column).className == "tile9") {
-          swapTiles("cell" + row + column, "cell" + (row - 1) + column);
+          movePics("cell" + row + column, "cell" + (row - 1) + column);
           counter++;
           console.log(counter);
           solved();
@@ -105,7 +126,7 @@ function clickTile(row, column) {
       //Checking if white tile is below
       if (row < 3) {
         if (document.getElementById("cell" + (row + 1) + column).className == "tile9") {
-          swapTiles("cell" + row + column, "cell" + (row + 1) + column);
+          movePics("cell" + row + column, "cell" + (row + 1) + column);
           counter++;
           console.log(counter);
           solved();
@@ -114,8 +135,9 @@ function clickTile(row, column) {
         }
       }
     }
-  //}
+  }
 }
+
 
 function solved() { //Checks if all images are in theirs correct positions
   var ok = { cell11: "tile1", cell12: "tile2", cell13: "tile3", cell21: "tile4", cell22: "tile5", cell23: "tile6", cell31: "tile7", cell32: "tile8", cell33: "tile9" }
@@ -137,7 +159,20 @@ function solved() { //Checks if all images are in theirs correct positions
   if (bolean === 1) { finished(); }
 }
 
+
+
 function finished() {
-  document.getElementById("results-zone").innerHTML = "You won!!";
-  document.getElementById("movements").innerHTML = "You´ve done in: " + counter
+
+  security--;
+
+  document.getElementById("movements").innerHTML = "You Won";
+  document.getElementById("extra").innerHTML =  counter + " movements";
+  document.getElementById("button").innerHTML = '<button onClick="shuffle();">Play again</button> ';
+
+
+  timerTime.setMilliseconds(miliSeconds);
+  timerTime.setSeconds(seconds);
+  timerTime.setMinutes(minutes);
+
+  clearInterval(myTime);
 }
