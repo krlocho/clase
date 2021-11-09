1be2410db149f63fac59a83ed48233a0219c9756
krlocho <thekrl88@gmail.com>
2021-11-09 13:05:06 +0000

    Some fixing

--- ETIEL/proyecto_4/script.js
+++ ETIEL/proyecto_4/script.js
@@ -12,26 +12,26 @@ var minutes = 0;
 var myTime = "00:00:00"
 
 
-function timer() {   //This is the timer
-  var msAux, sAux, mAux;
+function timer() {   
+  var milisec, sec, min;
   miliSeconds += 2;
 
   if (miliSeconds >= 100) {
     seconds++;
     miliSeconds = 0;
   }
-  
+
   if (seconds > 59) {
     minutes++;
     seconds = 0;
   }
 
 
-  if (seconds < 10) { sAux = "0" + seconds; } else { sAux = seconds; }
-  if (minutes < 10) { mAux = "0" + minutes; } else { mAux = minutes; }
-  if (miliSeconds < 10) { msAux = "0" + miliSeconds; } else { msAux = miliSeconds; }
+  if (seconds < 10) { sec = "0" + seconds; } else { sec = seconds; }
+  if (minutes < 10) { min = "0" + minutes; } else { min = minutes; }
+  if (miliSeconds < 10) { milisec = "0" + miliSeconds; } else { milisec = miliSeconds; }
 
-  document.getElementById("timer").innerHTML = mAux + ":" + sAux + ":" + msAux;
+  document.getElementById("timer").innerHTML = min + ":" + sec + ":" + milisec;
 }
 
 
@@ -50,7 +50,7 @@ function resetValues() {
   miliSeconds = 0;
   seconds = 0;
   minutes = 0;
-  
+
 
   document.getElementById("movements").innerHTML = "Moves: " + counter;
 
@@ -68,19 +68,17 @@ function shuffle() {
   timer();
   myTime = setInterval(timer, 20)
 
-  //coment this to don move the tiles randomly 
+ /*  //coment this to start without shuffling 
+  //access to each cell of the 3x3 grid
+  for (var row = 1; row <= 3; row++) { //For each row of the 3x3 grid
+    for (var column = 1; column <= 3; column++) { //For each column in this row
 
+      var row2 = Math.floor(Math.random() * 3 + 1); //Pick a random row from 1 to 3
+      var column2 = Math.floor(Math.random() * 3 + 1); //Pick a random column from 1 to 3
 
-    //access to each cell of the 3x3 grid
-    for (var row = 1; row <= 3; row++) { //For each row of the 3x3 grid
-      for (var column = 1; column <= 3; column++) { //For each column in this row
-  
-        var row2 = Math.floor(Math.random() * 3 + 1); //Pick a random row from 1 to 3
-        var column2 = Math.floor(Math.random() * 3 + 1); //Pick a random column from 1 to 3
-  
-        movePics("cell" + row + column, "cell" + row2 + column2); //Swap the look & feel of both cells
-      }
-    } 
+      movePics("cell" + row + column, "cell" + row2 + column2); //Swap the look & feel of both cells
+    }
+  } */
 }
 
 
@@ -107,7 +105,8 @@ function clickTile(row, column) {
         if (document.getElementById("cell" + row + (column - 1)).className == "tile9") {
           movePics("cell" + row + column, "cell" + row + (column - 1));
           counter++;
-          console.log(counter); solved();
+          console.log(counter); 
+          solved();
 
           return;
         }
@@ -141,22 +140,22 @@ function clickTile(row, column) {
 
 function solved() { //Checks if all images are in theirs correct positions
   var ok = { cell11: "tile1", cell12: "tile2", cell13: "tile3", cell21: "tile4", cell22: "tile5", cell23: "tile6", cell31: "tile7", cell32: "tile8", cell33: "tile9" }
-  var bolean = 0
+  var boolean = 0
   for (var i = 1; i <= 3; i++) {
     for (var j = 1; j <= 3; j++) {
       let tile = document.getElementById("cell" + i + j).classList
       let cell = "cell" + i + j;
       if (ok[cell] === tile[0]) {
-        bolean = 1;
+        boolean = 1;
       }
       else {
-        bolean = 0;
+        boolean = 0;
         break;
       }
     }
-    if (bolean === 0) { break; }
+    if (boolean === 0) { break; }
   }
-  if (bolean === 1) { finished(); }
+  if (boolean === 1) { finished(); }
 }
 
 
@@ -166,7 +165,7 @@ function finished() {
   security--;
 
   document.getElementById("movements").innerHTML = "You Won";
-  document.getElementById("extra").innerHTML =  counter + " movements";
+  document.getElementById("extra").innerHTML = counter + " movements";
   document.getElementById("button").innerHTML = '<button onClick="shuffle();">Play again</button> ';
 
 
