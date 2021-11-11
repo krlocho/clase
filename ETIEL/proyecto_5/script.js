6f974ae9513458a02d4fb1b411cf05afe603e443
krlocho <thekrl88@gmail.com>
2021-11-11 13:51:24 +0000

    SEARCH BAR

--- ETIEL/proyecto_5/script.js
+++ ETIEL/proyecto_5/script.js
@@ -1,2 +1,16 @@
-wipeout = document.querySelector('#Etiel.js');
-console.log(wipeout.hasChildNodes());
+const list = document.querySelector('ul');
+// filter files
+const searchBar = document.forms['searchBar'].querySelector('input');
+searchBar.addEventListener('keyup',(e)=>{// FAT ARROW FUNCTION
+  const term = e.target.value.toLowerCase();// to insure matches
+  const files = list.getElementsByTagName('li');
+    
+  Array.from(files).forEach(function (exer){ //FOR EACH instead of for loop
+    const title = exer.firstElementChild.textContent;
+    if(title.toLowerCase().indexOf(term) == -1){ //-1 means not present
+      exer.style.display = 'none';
+    } else {
+      exer.style.display = 'block';
+    }
+  });
+});
