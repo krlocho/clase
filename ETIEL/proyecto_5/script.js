7207c647ad0904bc876c6c908cbfeb72ecfa1988
krlocho <thekrl88@gmail.com>
2021-11-12 10:01:26 +0000

    FIXING THE SEARCH BAR AND ADDING THE DELETE FUNCTION

--- ETIEL/proyecto_5/script.js
+++ ETIEL/proyecto_5/script.js
@@ -6,11 +6,25 @@ searchBar.addEventListener('keyup',(e)=>{// FAT ARROW FUNCTION
   const files = list.getElementsByTagName('li');
     
   Array.from(files).forEach(function (exer){ //FOR EACH instead of for loop
-    const title = exer.firstElementChild.textContent;
+    const title = exer.textContent;
     if(title.toLowerCase().indexOf(term) == -1){ //-1 means not present
       exer.style.display = 'none';
     } else {
       exer.style.display = 'block';
     }
   });
+});
+
+
+
+// delete exercises
+list.addEventListener('click', function(e) {
+  if(e.target.className == 'delete'){
+    const li = e.target.parentElement;
+    if (li.className = "archivo"){
+    li.parentNode.removeChild(li);} //removes the element
+   //li.setAttribute ('style', 'display: none');//Hides the element 
+  // li.style.display="none"; 
+    //https://www.w3schools.com/jsref/prop_style_display.asp
+  }
 });
