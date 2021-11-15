730c8e62b91bca4c244f14b317b9700c4f271375
krlocho <thekrl88@gmail.com>
2021-11-15 09:45:14 +0000

    FIXING LITTLE THINGS

--- ETIEL/proyecto_5/script.js
+++ ETIEL/proyecto_5/script.js
@@ -1,4 +1,7 @@
 const list = document.querySelector('ul');
+const addForm=document.forms["add-ex"];
+
+
 // filter files
 const searchBar = document.forms['searchBar'].querySelector('input');
 searchBar.addEventListener('keyup',(e)=>{// FAT ARROW FUNCTION
@@ -28,3 +31,29 @@ list.addEventListener('click', function(e) {
     //https://www.w3schools.com/jsref/prop_style_display.asp
   }
 });
+
+
+addForm.querySelector("button").addEventListener('click', function(e){
+ e.preventDefault();//https://www.w3schools.com/tags/att_button_type.asp
+
+  // create elements
+  const value = addForm.querySelector('input[type="text"]').value;
+  const li = document.createElement('li');
+  const ExName = document.createElement('span');
+  const deleteBtn = document.createElement('span');
+
+  // add text content
+  ExName.textContent = value;
+  deleteBtn.textContent = 'delete';
+  
+    // add classes
+  ExName.classList.add('name');
+  deleteBtn.classList.add('delete');
+
+
+  // append to DOM
+  e.target.parentElement.parentElement.appendChild(li);
+  e.target.parentElement.parentElement.appendChild(ExName);
+  e.target.parentElement.parentElement.appendChild(deleteBtn);
+ 
+  });
